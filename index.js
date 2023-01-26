3a356b07b635ba8f4452293263c280a3d64c2033
Annie-LAN <ainingbehappy@gmail.com>
2023-01-25 20:28:39 -0600

    comment on the functions

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 const sqlite3 = require('sqlite3').verbose();
+
+// start the server
+// npm install
+// npm start
+// http://localhost:3000/
+
 
 app.get('/database', (req, res) => {
     const db = new sqlite3.Database('../../../Downloads/track_metadata.db', (err) => {
@@ -33,10 +38,15 @@ app.get('/artist', (req, res) => {
     res.send('Lists every artist')
   })
 
+// here :artistId is a variable
+// params: only contains any parameters passed to the route
 app.get('/artist/id/:artistId', (req, res) => {
     res.send('List every song where artist is ' + req.params.artistId)
 })
 
+// notice the change in the url compared to the previous one: avoid mistaking the path for a variable
+// query: contains only the keys and values related to the query on the request route (key=value)
+// start after the question mark in any URL. And if there are more than one, they are separated with the ampersand
 app.get('/artist/search', (req, res) => {
     res.send('List every song where artist is ' + req.query.artist + ' and bpm is > ' + req.query.bpm)
 })
