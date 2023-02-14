38345f7c374fda270ed2aad0ac68cacba8b5f7c5
Annie-LAN <ainingbehappy@gmail.com>
2023-02-13 22:27:08 -0600

    Update index.js
    change the filter

--- index.js
+++ index.js
@@ -45,35 +45,146 @@ app.get('/random', (req, res) => {
   db.serialize(() => {
     // "SELECT * FROM songs WHERE key = '" + key + "' ORDER BY RANDOM() LIMIT 10 "
       // Object.keys(req.query).length
-      // if (req.query.key) {
       let sql = "SELECT * FROM songs";
 
       if (Object.keys(req.query).length > 0) {
-        var attributes = Object.keys(req.query);
+        added_anything = false;
+        
+        ///// check through all the attributes
+        // artist_name
+        if (req.query.artist_name && added_anything == false) {
+          sql += " WHERE artist_name = '" + req.query.artist_name + "'";
+          added_anything = true;          
+        } else if (req.query.artist_name && added_anything == true) {
+          sql += " AND artist_name = '" + req.query.artist_name + "'";
+        } 
 
-        for (let i = 0; i < attributes.length; i++) {
-          if (i == 0) {sql += " WHERE "
-          } else {        
-            sql += " AND " 
-            }
-                   
-          // special cases: tempo (low, high), duration (low, high), loudness (low, high)
-          if (attributes[i] == 'tempo_low' && attributes[i+1] == 'tempo_high') {
-            sql += " tempo BETWEEN " + req.query[attributes[i]] + " AND '" + req.query[attributes[i+1]] + "' "
-            i++;
-          }else if(attributes[i] == 'duration_low' && attributes[i+1] == 'duration_high') {
-            sql += " duration BETWEEN " + req.query[attributes[i]] + " AND '" + req.query[attributes[i+1]] + "' "
-            i++;
-          }
-          else if(attributes[i] == 'loudness_low' && attributes[i+1] == 'loudness_high') {
-            sql += " loudness BETWEEN " + req.query[attributes[i]] + " AND '" + req.query[attributes[i+1]] + "' "
-            i++;
-          }
-          else{ 
-            sql += attributes[i] + " = '" + req.query[attributes[i]] + "' "
-          }   
-          
+        // title
+        if (req.query.title && added_anything == false) {
+          sql += " WHERE title = '" + req.query.title + "'";
+          added_anything = true;          
+        } else if (req.query.title && added_anything == true) {
+          sql += " AND title = '" + req.query.title + "'";
+        } 
+      
+        // key
+        if (req.query.key && added_anything == false) {
+          sql += " WHERE key = '" + req.query.key + "'";
+          added_anything = true;          
+        } else if (req.query.key && added_anything == true) {
+          sql += " AND key = '" + req.query.key + "'";
+        } 
+
+        // mode
+        if (req.query.mode && added_anything == false) {
+          sql += " WHERE mode = '" + req.query.mode + "'";
+          added_anything = true;          
+        } else if (req.query.mode && added_anything == true) {
+          sql += " AND mode = '" + req.query.mode + "'";
         }
+        
+        // time_signature
+        if (req.query.time_signature && added_anything == false) {
+          sql += " WHERE time_signature = '" + req.query.time_signature + "'";
+          added_anything = true;          
+        } else if (req.query.time_signature && added_anything == true) {
+          sql += " AND time_signature = '" + req.query.time_signature + "'";
+        } 
+
+        // year
+        if (req.query.year && added_anything == false) {
+          sql += " WHERE year = '" + req.query.year + "'";
+          added_anything = true;          
+        } else if (req.query.year && added_anything == true) {
+          sql += " AND year = '" + req.query.year + "'";
+        } 
+
+        // release
+        if (req.query.release && added_anything == false) {
+          sql += " WHERE release = '" + req.query.release + "'";
+          added_anything = true;          
+        } else if (req.query.release && added_anything == true) {
+          sql += " AND release = '" + req.query.release + "'";
+        } 
+
+        // tempo
+        // tempo_low exists
+        if (req.query.tempo_low && added_anything == false) {
+          if (req.query.tempo_high) {
+            sql += " WHERE tempo BETWEEN '" + req.query.tempo_low + "' AND '" + req.query.tempo_high + "'";
+            added_anything = true;
+          } else {
+          sql += " WHERE tempo >= '" + req.query.tempo_low + "'";
+          added_anything = true;          
+          }
+        } else if (req.query.tempo_low && added_anything == true) {
+          if (req.query.tempo_high) {
+            sql += " AND tempo BETWEEN '" + req.query.tempo_low + "' AND '" + req.query.tempo_high + "'";
+          } else {
+          sql += " AND tempo >= '" + req.query.tempo_low + "'";
+          }
+        } // tempo_low doesn't exist
+        else if (req.query.tempo_high && added_anything == false) {
+          sql += " WHERE tempo <= '" + req.query.tempo_high + "'";
+          added_anything = true;          
+        } else if (req.query.tempo_high && added_anything == true) {
+          sql += " AND tempo <= '" + req.query.tempo_high + "'";
+          added_anything = true;
+        }
+        
+
+        // duration
+        // duration_low exists
+        if (req.query.duration_low && added_anything == false) {
+          if (req.query.duration_high) {
+            sql += " WHERE duration BETWEEN '" + req.query.duration_low + "' AND '" + req.query.duration_high + "'";
+            added_anything = true;
+          } else {
+          sql += " WHERE duration >= '" + req.query.duration_low + "'";
+          added_anything = true;          
+          }
+        } else if (req.query.duration_low && added_anything == true) {
+          if (req.query.duration_high) {
+            sql += " AND duration BETWEEN '" + req.query.duration_low + "' AND '" + req.query.duration_high + "'";
+          } else {
+          sql += " AND duration >= '" + req.query.duration_low + "'";
+          }
+        } // duration_low doesn't exist
+        else if (req.query.duration_high && added_anything == false) {
+          sql += " WHERE duration <= '" + req.query.duration_high + "'";
+          added_anything = true;          
+        } else if (req.query.duration_high && added_anything == true) {
+          sql += " AND duration <= '" + req.query.duration_high + "'";
+          added_anything = true;
+        }
+ 
+        
+        // loudness
+        // loudness_low exists
+        if (req.query.loudness_low && added_anything == false) {
+          if (req.query.loudness_high) {
+            sql += " WHERE loudness BETWEEN '" + req.query.loudness_low + "' AND '" + req.query.loudness_high + "'";
+            added_anything = true;
+          } else {
+          sql += " WHERE loudness >= '" + req.query.loudness_low + "'";
+          added_anything = true;          
+          }
+        } else if (req.query.loudness_low && added_anything == true) {
+          if (req.query.loudness_high) {
+            sql += " AND loudness BETWEEN '" + req.query.loudness_low + "' AND '" + req.query.loudness_high + "'";
+          } else {
+          sql += " AND loudness >= '" + req.query.loudness_low + "'";
+          }
+        } // loudness_low doesn't exist
+        else if (req.query.loudness_high && added_anything == false) {
+          sql += " WHERE loudness <= '" + req.query.loudness_high + "'";
+          added_anything = true;          
+        } else if (req.query.loudness_high && added_anything == true) {
+          sql += " AND loudness <= '" + req.query.loudness_high + "'";
+          added_anything = true;
+        }
+
+
       }
 
       sql += " ORDER BY RANDOM() LIMIT 10;"
